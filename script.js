4b2a5d0b656128bed96d67b44186642db15610f5
BartekDarocha <bartlomiejdarocha@gmail.com>
2021-08-04 21:25:27 +0200

    losowanie liczb

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log('hello world');
+const button = document.querySelector('#button');
+const container = document.querySelector('#container'); 
+const rolledNumbers = [];
+let printDataValue = '';
+let disabled = false;
+
+function rollNumber() {
+  const min = 1;
+  const max = 60
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+function showWarningEnd() {
+  return '<br> To jest 6 liczb wylosowanych';
+}
+
+function setDateLine() {
+  let tempLine = ''
+  rolledNumbers.forEach((number, index) => {
+    tempLine += ` ${number}`;
+    if ((index + 1) !== rolledNumbers.length) {
+      tempLine += `,`;
+    }
+  });
+  return tempLine;
+}
+
+function checkRolledNumbers() {
+  if (disabled) {
+    return;
+  }
+  if (rolledNumbers.length < 6) {
+    rolledNumbers.push(rollNumber());
+    printDataValue = setDateLine();
+  } else {
+    printDataValue += showWarningEnd();
+    disabled = true;
+  }
+  container.innerHTML = printDataValue; 
+}
+
+button.addEventListener('click', checkRolledNumbers);
